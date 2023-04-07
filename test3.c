df48046f85fd38943825d82e9eb298bbb959358b
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-04-07 17:37:08 +0530

    Add files via upload

--- test3.c
+++ test3.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    // Convert cm to foot
+int main()
+{ /*this is a code for ur exam and ur marks are displayed. this code helps you get ur reward*/
+    printf("Your Reward is based on  your performance\n");
+    printf("Enter your marks\n");
 
+    int marks;
+    scanf("%d", &marks);
 
-    printf("Hello World\n");
+    if (marks >= 30)
+    {
+        printf("You passsed both subject and rewarded chocolate\n");
+    }
 
-    float l;
-    printf("Enter Length in cm\n");
-    scanf("%f",&l);
+    else if (marks > 15)
+    {
+        printf("You passed only one subject and rewarded 5 candies\n");
 
-    printf("Value of input length in foot is %.4f\n", l/30.48);
-    printf("%.2f = %.2f",l, l/30.48);
+        int a;
+        printf("select ur subject, 1 for maths and 2 for science\n");
+        scanf("%d", &a);
+
+        if (a == 1)
+        {
+            printf("You have passed maths\n");
+            printf("You have failed science\n");
+        }
+
+        if (a == 2)
+        {
+            printf("You have passed science\n");
+            printf("You have failed maths\n");
+        }
+    }
+    else if (marks >= 0)
+    {
+        printf("You have Failed and you are not getting any reward, bring your parents to the school");
+    }
 
     return 0;
 }
