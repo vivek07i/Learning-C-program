a8d86190f72a83d45df5e2beed84627ded6fec62
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-04-14 15:30:59 +0530

    Add files via upload

---
+++ riya.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x=1;
+    printf("Enter 1 to Get a Message\n");
+    scanf("%d", &x);
+
+    if (x=1)
+    {
+        printf("I love you Riya Sharma\n");
+    }
+    
+    
+    return 0;
+}
