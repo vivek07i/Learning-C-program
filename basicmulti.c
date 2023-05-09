5bb78102110032616f5197ce6b788459911e7d01
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-05-09 18:22:54 +0530

    Add files via upload

---
+++ basicmulti.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    printf("The Table of %d :\n", n);
+    for (int i = 1; i <= 10; ++i)
+    {
+
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    return 0;
+}
