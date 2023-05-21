a46376c6c51e5ba4d6c85fe3f3759284f67bba98
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-05-21 21:19:55 +0530

    Add files via upload

---
+++ rhombus.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows ;
+
+    printf("Enter The number of pattern You want : \n");
+    scanf("%d",&rows);
+    
+    // first outer loop to iterate through each row
+    for (int i = 0; i < rows; i++)
+    {
+
+        // first inner loop to print white spaces
+        for (int j = 0; j < rows - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        // second inner loop to print * star in each row
+        for (int k = 0; k < rows; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
