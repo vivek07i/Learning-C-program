d0fe79eaaac9443f719f802707d03670bcabbf0e
Vivek <129210903+vivek07i@users.noreply.github.com>
2023-07-19 01:02:52 -0700

    Add files via upload

---
+++ arraysth.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nEnter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("\n\nEnter the %d elements of the array: \n\n", size);
+    for (i = 0; i < size; i++)
+        scanf("%d", &a[i]);
+
+    big = a[0]; // initializing
+    /*
+        from 2nd element to the last element
+        find the bigger element than big and
+        update the value of big
+    */
+    for (i = 1; i < size; i++)
+    {
+        if (big < a[i]) // if larger value is encountered
+        {
+            big = a[i]; // update the value of big
+        }
+    }
+    printf("\n\nThe largest element is: %d", big);
+
+    small = a[0]; // initializing
+    /*
+        from 2nd element to the last element
+        find the smaller element than small and
+        update the value of small
+    */
+    for (i = 1; i < size; i++)
+    {
+        if (small > a[i]) // if smaller value is encountered
+        {
+            small = a[i]; // update the value of small
+        }
+    }
+    printf("\n\nThe smallest element is: %d", small);
+
+    return 0;
+}
